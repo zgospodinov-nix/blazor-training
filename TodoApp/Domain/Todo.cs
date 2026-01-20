bc7756e84261fe20563a9da403ca4f519de678ff
Zdravko Gospodinov <zgospodinov@n-ix.com>
2026-01-20 15:15:05 +0200

    Domain project and Todo model with Data Annotation validations

---
+++ TodoApp/Domain/Todo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class Todo
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(2000)]
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime CreatedDate { get; init; } = DateTime.UtcNow;
+
+    public DateTime? DueDate { get; set; }
+
+    public void ToggleCompletion() => IsCompleted = !IsCompleted;
+}
